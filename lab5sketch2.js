6202074be80ed34a3ec0f6a241227a089883cfa3
jharttt <124187324+jharttt@users.noreply.github.com>
2023-03-01 12:42:27 -0500

    Create lab5sketch2.js
    https://editor.p5js.org/john.hart/sketches/ram4jm46q

---
+++ lab5sketch2.js
@@ -0,0 +1,37 @@
+let catcherX; // horizontal
+let speedX = 2; // speed
+let speedY = 1; // speed
+let catcherY = 0;  // vertical
+function setup() {
+  createCanvas(400, 400);
+  catcherX = random(400); //randomizes x so it changes every time
+}
+
+function draw() {
+  background(220);
+  fill(0, 0, 255); // blue bear
+  ellipse(catcherX, catcherY, 80, 80); // body
+  ellipse(catcherX - 33, catcherY - 28, 20, 20); // left ear
+  ellipse(catcherX + 33, catcherY - 28, 20, 20); // right ear
+  catcherX += speedX; // adds speed to x variable
+		catcherY += speedY; // to y variable
+    if (catcherX > width || catcherX <= 0){ // condition that keeps bear in bounds
+     speedX *= -1;
+  }
+      if (catcherY > height || catcherY <= 0){ // same for y variable
+        speedY *= -1;
+  }
+  
+}
+function keyPressed(){
+    if (keyCode === UP_ARROW) {
+    catcherY = catcherY - 30;
+  } else if (keyCode === DOWN_ARROW) {
+   catcherY = catcherY + 30;
+  }
+  if (keyCode === LEFT_ARROW) {
+    catcherX = catcherX - 15;
+  } else if (keyCode === RIGHT_ARROW) {
+    catcherX = catcherX + 15;
+  }
+  }
